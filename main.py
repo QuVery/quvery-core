ef7878db8c0c4c241ec491f15e40aee9728074f7
Omid Saadat <omid3098@gmail.com>
2023-11-09 16:32:43 +0300

    Remove unnecessary imports and read factory settings.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
 import bpy
-import os
 from server import start_server
-from utils import ArgumentParser
 from utils.logger import logger
 
 bpy.ops.wm.read_factory_settings(use_empty=True)
