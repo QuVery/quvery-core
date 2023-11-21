b74d54fa393cd1ace7fb3e27218a9b145755aca8
Omid Saadat <omid3098@gmail.com>
2023-11-21 23:27:08 +0300

    Add - rule to check transform is applied -close #20

---
+++ rules/3d/check/08_is_transform_applied.py
@@ -0,0 +1,37 @@
+
+# imports here
+import bpy
+
+# necessary for the rule to be loaded
+RULE_NAME = "Is Transform Applied"
+
+
+def process(input):
+    """
+    This function is called for each file that is checked. The input is the file path.
+    the function should return an empty json if the file is valid and a json object with the status and required information.
+    status can be one of the following: "error", "warning", "info"
+    example: 
+    for a single error: {"status": "error" , "details": {"object_name": "error message"}}
+    for multiple errors: {"status": "error" , "details": {"object_name": "error message", "object_name2": "error message2"}}
+    for a single warning: {"status": "warning" , "details": {"object_name": "warning message"}}
+    for info: {"status": "info" , "details": {"object_name": "info message"}}
+    """
+
+    result_json = {"status": "error"}  # default status is info
+    details_json = {}
+
+    # Implement your rule here and fill the details_json with the appropriate information or errors or leave it empty if there are no errors or warnings
+    for obj in bpy.data.objects:
+        if obj.type == 'MESH':
+            # Check if the scale and rotation are applied
+            if obj.scale != (1, 1, 1):
+                details_json[obj.name] = "Scale is not applied"
+            if obj.rotation_euler != (0, 0, 0):
+                details_json[obj.name] = "Rotation is not applied"
+
+    if details_json != {}:
+        result_json["details"] = details_json
+        return result_json
+    else:
+        return {}
